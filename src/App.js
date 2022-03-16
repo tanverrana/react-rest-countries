e7f053a6abefe6fa8c1bb4b5dcfa6700f310e87e
tanverrana <tanversobur@gmail.com>
2022-03-16 23:31:59 +0600

    country name

--- src/App.js
+++ src/App.js
@@ -22,8 +22,17 @@ function LoadCountries() {
       <h1>Visiting Every Country Of the world.</h1>
       <h3>Available Countries: {countries.length}</h3>
       {
-        countries.map(country => <p>{country.name.common}</p>)
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
       }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Country Name:<span className="country-name">{props.name}</span></h2>
+      <h2>Population:<span className="country-name">{props.population}</span></h2>
     </div>
   )
 }
