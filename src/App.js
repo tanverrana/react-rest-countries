24aea5ef65d873f510f9e026e7e4a3d876d06300
tanverrana <tanversobur@gmail.com>
2022-03-16 23:04:18 +0600

    create loadcountries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+function LoadCountries() {
+  const [] = useState
+  return (
+    <div>
+      <h1>Visiting Every Country Of the world.</h1>
+    </div>
+  )
 }
 
 export default App;
