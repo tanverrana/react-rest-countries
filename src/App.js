041fb9d3c2596a55c33a192b51a02d5a441017ae
tanverrana <tanversobur@gmail.com>
2022-03-16 23:11:06 +0600

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
@@ -10,10 +11,16 @@ function App() {
   );
 }
 function LoadCountries() {
-  const [] = useState
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
   return (
     <div>
       <h1>Visiting Every Country Of the world.</h1>
+      <h3>Available Countries: {countries.length}</h3>
     </div>
   )
 }
